05864654c735ed83dab2768ea7c8bb706898a828
Daniel <dm858@york.ac.uk>
2020-12-04 01:49:56 +0100

    Created input reading utility with header structure

--- 1/day1.cpp
+++ 1/day1.cpp
@@ -1,1 +1,15 @@
-#hello
+#include<iostream>
+#include<vector>
+#include"utils.h"
+
+int main(){
+
+   std::vector<std::string> input = read_input("input", "");
+
+   for (int i=0; i<input.size(); i++){
+      std::cout << input[i] << std::endl;
+   }
+
+   return 0;
+
+}
---
+++ 1/utils.h
@@ -0,0 +1,9 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#include<vector>
+#include<string>
+
+std::vector<std::string> read_input(std::string file_name, std::string separator);
+
+#endif /* UTILS_H */
