2f4e054b16eba66b7bfe78312924869c4d72a798
Daniel <dm858@york.ac.uk>
2020-12-04 02:33:51 +0100

    Finished day 1.

--- 1/day1.cpp
+++ 1/day1.cpp
@@ -4,10 +4,21 @@
 
 int main(){
 
-   std::vector<std::string> input = read_input("input", "");
+   std::vector<int> input = input_to_int(read_input("input", ""));
 
-   for (int i=0; i<input.size(); i++){
-      std::cout << input[i] << std::endl;
+   // for (int i=0; i<input.size(); i++){
+   //    std::cout << input[i] << std::endl;
+   // }
+
+   for (int i=0; i<input.size()-2; i++){
+      for (int j=i+1; j<input.size()-1; j++){
+         for (int k=j+1; k<input.size(); k++){
+
+            if ( (input[i]+input[j]+input[k]) == 2020 ){
+               std::cout << input[i]*input[j]*input[k] << std::endl;
+            }
+         }
+      }
    }
 
    return 0;
