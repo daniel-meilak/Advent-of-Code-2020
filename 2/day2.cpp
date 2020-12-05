96fead64ac17ad3549e375e733c56b19ed0579c2
Daniel <dm858@york.ac.uk>
2020-12-05 01:13:29 +0100

    Day 2 part 1 finished

---
+++ 2/day2.cpp
@@ -0,0 +1,70 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include"utils.h"
+
+int main(){
+
+    // read input into vector of strings.
+    std::vector<std::string> input = read_input("input", "");
+
+    int pos, min, max, matches;
+    int valid = 0;
+    char letter;
+    std::string line, password;
+    std::string min_string, max_string;
+
+
+    // loop through each input
+    for (int i=0; i<input.size(); i++){
+
+        line = input[i];
+
+        // work through the line
+        pos=0;
+
+        // extract min number
+        while ( line[pos] != '-' ){
+            min_string.push_back(line[pos]);
+            pos++;
+        }
+        min = std::stoi(min_string);
+        pos++;
+
+        // extract max number
+        while ( line[pos] != ' ' ){
+            max_string.push_back(line[pos]);
+            pos++;
+        }
+        max = std::stoi(max_string);
+        pos++;
+
+        // extract letter requirement
+        letter = line[pos];
+        pos += 3;
+
+        // extract password
+        password = line.substr(pos, line.size()-pos);
+
+        // loop through password and check for number of occurences
+        matches = 0;
+        for (int j=0; j<password.size(); j++){
+            if ( password[j] == letter){
+                matches++;
+            }
+        }
+
+        // check if number of matches is between min and max;
+        if ( (matches >= min) && (matches <= max) ){
+            valid++;
+        }
+
+        // empty variables for next line;
+        min_string.clear();
+        max_string.clear();
+    }
+
+    std::cout << "Number of valid passwords: " << valid << std::endl;
+
+    return 0;
+}
