58768e7d6cef385886318daad9277dee69f2c1cb
Daniel <dm858@york.ac.uk>
2020-12-07 14:24:58 +0100

    Finished day 6

---
+++ 6/day6.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<algorithm>
+#include<cstdlib>
+#include"utils.h"
+
+int main(){
+
+    // read input into vector of strings.
+    std::vector<std::string> input = read_input("input", "");
+
+    // tally of questions answered
+    int tally = 0;
+    
+    // list of unique questions answered by the group
+    std::string questions = "";
+
+    for (int i=0; i<input.size(); i++){
+
+        std::string line;
+        
+        line = input[i];
+
+        for ( int j=0; j<line.size(); j++ ){
+
+            // if new question is answered, add it to questions list
+            if ( questions.find(line[j]) == std::string::npos ){
+                questions.push_back(line[j]);
+            }
+        } 
+
+        // Reach end of group
+        if ( (input[i+1] == "") || (i == input.size()-1) ){
+            tally += questions.size();
+            questions.clear();
+        }
+    }
+
+    std::cout << "Questions answered: " << tally << std::endl;
+
+    return 0;
+
+}
