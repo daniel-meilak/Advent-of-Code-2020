42e4c536182272d8050fa83eaf089fb11275cbc8
Daniel <dm858@york.ac.uk>
2020-12-12 03:10:42 +0100

    Finished day 11

---
+++ 10/day10_2.cpp
@@ -0,0 +1,77 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<algorithm>
+#include<cstdlib>
+#include<map>
+#include<list>
+#include"utils.h"
+
+int main(){
+
+    // read input into vector of ints.
+    std::vector<int> input = input_to_int(read_input("input", ""));
+
+    // sort plugs
+    std::sort(input.begin(), input.end());
+
+    int appliance_jolt = input[input.size()-1]+3;
+
+    // add socket and appliance to vector
+    input.push_back(appliance_jolt);
+    input.insert(input.begin(), 0);
+
+    
+
+    // create a map of adaptors and the sockets they link to
+    std::map<int, std::vector<int>> adaptor_links;
+
+    // add three extra adaptors that dont link to anything 
+    // to make map creation easier
+    input.push_back(1000);
+    input.push_back(1100);
+    input.push_back(1200);
+
+    // add all adaptors and their possible connections to adaptor_links
+    for ( int i = 0; i<input.size()-3; i++ ){
+        
+        // add adaptor
+        adaptor_links[input[i]] = {};
+
+        // add links (possibilities are the 3 next adaptors)
+        for ( int j = i+1; j<= i+3; j++){
+            if ( (input[j]-input[i]) <= 3 ){
+                adaptor_links[input[i]].push_back(input[j]);
+            }
+        }
+    }
+
+    // tally paths
+    long long int num_paths = 0;
+    // list of paths
+    // better than vector as we only access first and last member
+    std::list<int> possible_paths = {0};
+
+    while ( possible_paths.size() > 0 ){
+
+        // if the path gets to the end, incr path counter
+        // remove path and continue;
+        if ( possible_paths.front() == appliance_jolt ){
+            num_paths++;
+            possible_paths.pop_front();
+            continue;
+        }
+
+        // for each node in path, add branches to path list
+        for (int adaptor : adaptor_links[possible_paths.front()]){
+            possible_paths.push_back(adaptor);
+        }
+
+        // once node is exhausted, remove from path list
+        possible_paths.pop_front();
+    }
+
+    std::cout << "Number of possible adapter chains: " << num_paths << std::endl;
+
+    return 0;
+}
---
+++ 11/day11v2_part2.cpp
@@ -0,0 +1,242 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<algorithm>
+#include<cstdlib>
+#include"utils.h"
+
+int main(){
+
+    // read input into vector of strings.
+    std::vector<std::string> input = read_input("input", "");
+
+    // add boundary to system
+
+    // to top and bottom line
+    std::string top_bottom(input[0].size()+2,'.');
+    input.insert(input.begin(), top_bottom);
+    input.push_back(top_bottom);
+
+    // to left and right borders
+    for (int i=1; i<input.size()-1; i++){
+        input[i].insert(input[i].begin(),'.');
+        input[i].push_back('.');
+    }
+
+    // create copy
+    std::vector<std::string> copy = input; 
+    bool running = true;
+    int iter = 0;
+
+    while (running){
+        // now that we have border, can treat all relevant cells as the centre
+        // loop through whole system except border
+        for (int y = 1; y<input.size()-1; y++){
+            for (int x = 1; x<input[0].size()-1; x++){
+
+                // if not seat, ignore position
+                if ( copy[y][x] == '.' ){
+                    continue;
+                }
+
+                int full_count = 0;
+                std::vector<bool> occupied(8,false);
+                
+                // directions
+                int a,b;
+
+                // west
+                a = x+1;
+                while ( true ){
+
+                    if ( copy[y][a] == '#' ){
+                        occupied[0] = true;
+                        break;
+                    }
+                    else if (copy[y][a] == 'L'){
+                        break;
+                    }
+
+                    if ( a == copy[0].size()-1 ){
+                        break;
+                    }
+                    a++;
+                }
+
+                // east
+                a = x-1;
+                while ( true ){
+
+                    if ( copy[y][a] == '#' ){
+                        occupied[1] = true;
+                        break;
+                    }
+                    else if (copy[y][a] == 'L'){
+                        break;
+                    }
+
+                    if ( a == 0 ){
+                        break;
+                    }
+                    a--;
+                }
+
+                // north
+                a = y-1;
+                while ( true ){
+
+                    if ( copy[a][x] == '#' ){
+                        occupied[2] = true;
+                        break;
+                    }
+                    else if (copy[a][x] == 'L'){
+                        break;
+                    }
+
+                    if ( a == 0 ){
+                        break;
+                    }
+                    a--;
+                }
+
+                // south
+                a = y+1;
+                while ( true ){
+
+                    if ( copy[a][x] == '#' ){
+                        occupied[3] = true;
+                        break;
+                    }
+                    else if (copy[a][x] == 'L'){
+                        break;
+                    }
+
+                    if (a == copy.size()-1 ){
+                        break;
+                    }
+                    a++;
+                }
+
+                // north east
+                a = y-1;
+                b = x+1;
+                while ( true ){
+
+                    if (copy[a][b] == '#' ){
+                        occupied[4] = true;
+                        break;
+                    }
+                    else if (copy[a][b] == 'L'){
+                        break;
+                    }
+
+                    if ( (a == 0) || (b == copy[0].size()-1) ){
+                        break;
+                    }
+                    a--;
+                    b++;
+                }
+
+                // north west
+                a = y-1;
+                b = x-1;
+                while ( true ){
+
+                    if (copy[a][b] == '#' ){
+                        occupied[5] = true;
+                        break;
+                    }
+                    else if (copy[a][b] == 'L'){
+                        break;
+                    }
+
+                    if ( (a == 0) || (b == 0) ){
+                        break;
+                    }
+                    a--;
+                    b--;
+                }
+
+                // south east
+                a = y+1;
+                b = x+1;
+                while ( true ){
+
+                    if (copy[a][b] == '#' ){
+                        occupied[6] = true;
+                        break;
+                    }
+                    else if (copy[a][b] == 'L'){
+                        break;
+                    }
+
+                    if ( (a == copy.size()-1) || (b == copy[0].size()-1) ){
+                        break;
+                    }
+                    a++;
+                    b++;
+                }
+
+                // south west
+                a = y+1;
+                b = x-1;
+                while ( true ){
+                    
+                    if (copy[a][b] == '#' ){
+                        occupied[7] = true;
+                        break;
+                    }
+                    else if (copy[a][b] == 'L'){
+                        break;
+                    }
+
+                    if ( (a == copy.size()-1) || (b == 0) ){
+                        break;
+                    }
+                    a++;
+                    b--;
+                }
+
+                // tally full seats
+                int tally = 0;
+                for (bool state : occupied){
+                    if (state){ tally++; }
+                }
+
+                if ( (copy[y][x] == '#') && (tally >= 5) ){
+                    input[y][x] = 'L';
+                }
+                else if ( (copy[y][x] == 'L') && (tally == 0) ){
+                    input[y][x] = '#';
+                }
+
+            }
+        }
+
+        if ( input == copy ){
+
+            int tally = 0;
+
+            // simulation has stabilised
+            for (int i=1; i<input.size()-1; i++){
+                for (int j=1; j<input[0].size()-1; j++){
+
+                    if (input[i][j] == '#'){
+                        tally++;
+                    } 
+                }
+            }
+            
+            std::cout << "After " << iter << " iterations." << std::endl;
+            std::cout << "Number of occupied seats: " << tally << std::endl;
+            running = false;
+        }
+        else {
+            iter++;
+            copy = input;
+        }
+    }
+
+
+    return 0;
+}
