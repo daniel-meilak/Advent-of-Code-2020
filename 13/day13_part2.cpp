833bbf3ed2c2d93dfe12fbbc98679f86536665a3
Daniel <dm858@york.ac.uk>
2020-12-14 03:06:15 +0100

    Finished day 13

--- 13/day13_part2.cpp
+++ 13/day13_part2.cpp
@@ -5,17 +5,31 @@
 #include<cstdlib>
 #include<utility>
 #include"../utils/utils.h"
+#include"../utils/math_func.h"
 
 int main(){
 
     // read input into vector of strings.
     std::vector<std::string> input = read_input("input", ",");
 
+    // vectors to store bus line and index
+    std::vector<long long int> bus_line, index; 
+
+    for (int i=1; i<input.size(); i++){
+        
+        // ignore "x"
+        if ( input[i] == "x" ){ continue; }
+
+        int line = std::stoi(input[i]);
+
+        bus_line.push_back(line);
+        index.push_back( line- ((i-1) % line) );
+    }
+
+    long long int answer = chinese_remainder(bus_line, index);
     
+    std::cout << "Answer (part 2): " << answer << std::endl;
 
     return 0;
 }
 
-// Modular Multiplicative inverse
-// 
-int mod_mul_inv
