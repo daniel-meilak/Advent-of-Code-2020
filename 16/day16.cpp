c14c328e10b1a6fc06731482ba2c5f71a2266a62
Daniel <dm858@york.ac.uk>
2020-12-16 19:16:21 +0100

    Finished day 16

--- 16/day16.cpp
+++ 16/day16.cpp
@@ -2,46 +2,94 @@
 #include<vector>
 #include<string>
 #include<algorithm>
-#include<utility>
+#include<regex>
 #include<cstdlib>
 #include"../utils/utils.h"
 
 class rule_t{
 public:
     std::string name;
-    std::pair<int,int> low;
-    std::pair<int,int> high;
+    std::vector<int> low;
+    std::vector<int> high;
 };
 
 int main(){
 
-    // read input into vector of strings.
-    std::vector<std::string> input = read_input("input", "");
+    // read rules into vector of strings.
+    std::vector<std::string> rule_list = read_input("rules", "");
 
     // vector of rules
     std::vector<rule_t> rules;
 
-    // process rules from input, lines 0-19
-    for (int i=0; i<20; i++){
-        std::string line = input[i];
+    // regex for rule name and values
+    std::regex name("^[a-z ]+");
+    std::regex value("([0-9]+)-([0-9]+) or ([0-9]+)-([0-9]+)");
+
+    // mathing strings
+    std::smatch matches;
+
+    for ( std::string line : rule_list ){
+
+        // new rule 
         rule_t rule;
 
-        // all values are the same length
-        // can use ':' an an achor for substr
-        int pos = line.find(':');
-        rule.name = line.substr(0,pos);
+        // match name;
+        std::regex_search(line, matches, name);
+        rule.name = matches[0];
 
-        // low range 
-        rule.low.first   = std::stoi(line.substr(pos+2,2));
-        rule.low.second  = std::stoi(line.substr(pos+5,3));
-
-        // high range
-        rule.high.first  = std::stoi(line.substr(pos+12,3));
-        rule.high.second = std::stoi(line.substr(pos+16,3));
+        std::regex_search(line, matches, value);
+        rule.low.push_back(std::stoi(matches[1]));
+        rule.low.push_back(std::stoi(matches[2]));
+        rule.high.push_back(std::stoi(matches[3]));
+        rule.high.push_back(std::stoi(matches[4]));
 
         rules.push_back(rule);
     }
+    
+    // read nearby tickets
+    std::vector<int> tickets_together = input_to_int(read_input("nearby_tickets",","));
+    std::vector<std::vector<int>> tickets;
+    std::vector<int> single;
 
+    // sort tickets into vector of vector
+    for (int i=0; i<tickets_together.size(); i++){
+        single.push_back(tickets_together[i]);
+        if ( single.size() == 20 ){
+            tickets.push_back(single);
+            single.clear();
+        }
+    }
+
+    // check each ticket for validity
+    // add up all invalid values
+    int invalid = 0;
+    for (int i=0; i<tickets_together.size(); i++){
+
+        // check ticket i
+        int val = tickets_together[i];
+        bool validity = false;
+
+        // check against every rule 
+        for (int j=0; j<rules.size(); j++){
+
+            // if it is less the lower min or higher max -> invalid
+            if ( (val >= rules[j].low[0]) && (val <= rules[j].low[1]) ){
+                validity = true;
+                break;
+            }
+            // if it is higher than low max and lower than high min -> invalid
+            else if ( (val >= rules[j].high[0]) && (val <= rules[j].high[1]) ){
+                validity = true;
+                break;
+            }
+        }
+
+        if ( !validity ){
+            invalid += val;
+        }
+    }
+    
+    std::cout << "Sum of invalid values: " << invalid << std::endl;
 
     return 0;
 }
---
+++ 16/day16part2.cpp
@@ -0,0 +1,130 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<algorithm>
+#include<regex>
+#include<cstdlib>
+#include"../utils/utils.h"
+
+class rule_t{
+public:
+    std::string name;
+    std::vector<int> low;
+    std::vector<int> high;
+};
+
+int main(){
+
+    // read rules into vector of strings.
+    std::vector<std::string> rule_list = read_input("rules", "");
+
+    // vector of rules
+    std::vector<rule_t> rules;
+
+    // regex for rule name and values
+    std::regex name("^[a-z ]+");
+    std::regex value("([0-9]+)-([0-9]+) or ([0-9]+)-([0-9]+)");
+
+    // mathing strings
+    std::smatch matches;
+
+    for ( std::string line : rule_list ){
+
+        // new rule 
+        rule_t rule;
+
+        // match name;
+        std::regex_search(line, matches, name);
+        rule.name = matches[0];
+
+        std::regex_search(line, matches, value);
+        rule.low.push_back(std::stoi(matches[1]));
+        rule.low.push_back(std::stoi(matches[2]));
+        rule.high.push_back(std::stoi(matches[3]));
+        rule.high.push_back(std::stoi(matches[4]));
+
+        rules.push_back(rule);
+    }
+    
+    // read nearby tickets
+    std::vector<int> tickets_together = input_to_int(read_input("nearby_tickets",","));
+    std::vector<std::vector<int>> tickets;
+    std::vector<int> single;
+
+    // sort tickets into vector of vector
+    for (int i=0; i<tickets_together.size(); i++){
+        single.push_back(tickets_together[i]);
+        if ( single.size() == 20 ){
+            tickets.push_back(single);
+            single.clear();
+        }
+    }
+
+    // check for invalid entries in each ticket
+    for (int k=0; k<tickets.size(); k++){
+        for (int i=0; i<tickets[i].size(); i++){
+
+            // check ticket i
+            int val = tickets[k][i];
+            bool validity = false;
+
+            // check against every rule 
+            for (int j=0; j<rules.size(); j++){
+
+                // if it is less the lower min or higher max -> invalid
+                if ( (val >= rules[j].low[0]) && (val <= rules[j].low[1]) ){
+                    validity = true;
+                    break;
+                }
+                // if it is higher than low max and lower than high min -> invalid
+                else if ( (val >= rules[j].high[0]) && (val <= rules[j].high[1]) ){
+                    validity = true;
+                    break;
+                }
+            }
+
+            // if a ticket entry is invalid, remove the ticket
+            if ( !validity ){
+                tickets.erase(tickets.begin()+k);
+                k--;
+            }
+        }
+    }
+
+    // loop through columns of data in tickets
+    for (int i=0; i<tickets[0].size(); i++){
+
+        // vector of booleans for each rule column applies to
+        std::vector<bool> validity(rules.size(),true);
+
+        for (int j=0; j<tickets.size(); j++){
+
+            int val = tickets[j][i];
+
+            // loop through rules
+            for (int k=0; k<rules.size(); k++){
+
+                // if it is less the lower min or higher max -> invalid
+                if ( (val < rules[k].low[0]) || (val > rules[k].high[1]) ){
+                    validity[k] = false;
+                }
+                // if it is higher than low max and lower than high min -> invalid
+                else if ( (val > rules[k].low[1]) && (val < rules[k].high[0]) ){
+                    validity[k] = false;
+                }
+            }
+        }
+
+        // print each rule column applies to
+        std::cout << "Column " << i+1 << " applies to " << std::count(validity.begin(), validity.end(), true) << " rule(s)\t";  
+        for (int r=0; r<validity.size(); r++){
+            if ( validity[r] ){ std::cout << r+1 << "\t"; }
+        }
+        std::cout << std::endl;
+
+        
+    }
+    
+
+    return 0;
+}
