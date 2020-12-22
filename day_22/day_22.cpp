b4d303600a7d55c09c4f89b77036740f4dcd339e
Daniel <daniel.mlak@gmail.com>
2020-12-22 15:13:53 +0100

    Started day 22

---
+++ day_22/day_22.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<algorithm>
+#include<cstdlib>
+#include"../utils/utils.h"
+
+int main(){
+
+    // read input into vector of strings.
+    std::vector<std::vector<std::string>> input = split_input(read_input("input", ""), "");
+
+    
+
+    return 0;
+}
