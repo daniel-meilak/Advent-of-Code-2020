2885ec6ddc1e885e6dd88f365992d6e132becf8e
Daniel <daniel.mlak@gmail.com>
2020-12-24 13:07:44 +0100

    Started day 24

---
+++ day_24/day_24.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<algorithm>
+#include<cstdlib>
+#include"../utils/utils.h"
+
+int main(){
+
+    // read input into vector of strings.
+    std::vector<std::string> input = read_input("input", "");
+
+    return 0;
+}
