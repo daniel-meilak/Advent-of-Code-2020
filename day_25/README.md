c607c1fa51c2dda0389792683bd4589537f36448
Daniel <daniel.mlak@gmail.com>
2020-12-25 16:46:54 +0100

    Finished day 25!

---
+++ day_25/README.md
@@ -0,0 +1,3 @@
+## Day 25
+
+I had so much fun this year taking part in Advent of Code. I'll probably go back and finish last years since I never did and eventually I'll take a look at the previous years ones!
---
+++ day_25/day_25.cpp
@@ -0,0 +1,65 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<algorithm>
+#include<cstdlib>
+#include"../utils/utils.h"
+
+long int handshake( long int subject_number, long int loop_size );
+
+int main(){
+
+    // read input into vector of strings.
+    std::vector<int> input = input_to_int(read_input("input", ""));
+
+    // specific loop values for door and card
+    int card_loop, door_loop;
+
+    int loop_max = 100000000;
+    int subject_number = 7;
+    int modulus = 20201227;
+
+    // trial public key and bool for ending loop
+    int public_key = 1;
+    bool card_found = false, door_found = false;
+
+    for (int i=1; i<loop_max; i++){
+
+        public_key *= subject_number;
+        public_key  = public_key % modulus;
+
+        if ( public_key == input[0] ){
+            card_loop = i;
+            card_found = true;
+            std::cout << "Card loop size: " << card_loop << std::endl;
+        }
+        else if ( public_key == input[1] ){
+            door_loop = i;
+            door_found = true;
+            std::cout << "Door loop size: " << door_loop << std::endl;
+        }
+
+        // if both keys are found, break
+        if (card_found && door_found){
+            break;
+        }
+    }
+
+    long int encryption_key = handshake(input[1], card_loop);
+
+    std::cout << "Encryption key: " << encryption_key << std::endl;
+
+    return 0;
+}
+
+long int handshake( long int subject_number, long int loop_size ){
+
+    long int output = 1UL;
+
+    for (int i=0; i<loop_size; i++){
+        output *= subject_number;
+        output = output % 20201227UL;
+    }
+
+    return output;
+}
