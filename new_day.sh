0fec15f1a82c0d8660efe267d98ba00775b12c28
Daniel <daniel.mlak@gmail.com>
2021-01-04 17:36:03 +0100

    Updated Utils and new_day.sh

--- new_day.sh
+++ new_day.sh
@@ -9,12 +9,19 @@ echo "#include<iostream>
 #include<string>
 #include<algorithm>
 #include<cstdlib>
-#include\"../Utils/utils.h\"
+#include\"../../Utils/utils.h\"
 
 int main(){
 
     // read input into vector of strings.
-    std::vector<std::string> input = read_input(\"input\", \"\");
+    // std::vector<std::string> input = read_input(\"input\", \"\");
+
+    // read input into vector of vector of strings.
+    // std::vector<std::string> delimiters = {};
+    // std::vector<std::vector<std::string>> input = read_input_2D(\"input\", delimiters);
+
+    std::cout << \"Answer (part 1): \" << std::endl;
+    std::cout << \"Answer (part 2): \" << std::endl;
 
     return 0;
 }" >> day_${1}/day_${1}.cpp
@@ -24,12 +31,9 @@ echo "#include<iostream>
 #include<string>
 #include<algorithm>
 #include<cstdlib>
-#include\"../Utils/utils.h\"
+#include\"../../Utils/utils.h\"
 
 int main(){
-
-    // read input into vector of strings.
-    //std::vector<std::string> input = read_input(\"input\", \"\");
 
     return 0;
 }" >> day_${1}/test.cpp
