5531500c02e01ad7b16b52a5058cd619e047e8be
Daniel <daniel.mlak@gmail.com>
2021-02-08 01:56:39 +0100

    Updated week 2

--- week_2/day_11/day_11.cpp
+++ week_2/day_11/day_11.cpp
@@ -11,7 +11,6 @@ int main(){
     std::vector<std::string> input = read_input("input", "");
 
     // add boundary to system
-
     // to top and bottom line
     std::string top_bottom(input[0].size()+2,'.');
     input.insert(input.begin(), top_bottom);
@@ -23,16 +22,21 @@ int main(){
         input[i].push_back('.');
     }
 
-    // create copy
-    std::vector<std::string> copy = input; 
-    bool running = true;
-    int iter = 0;
+    size_t height = input.size()-1;
+    size_t width  = input[0].size()-1;
 
-    while (running){
+    // create copy
+    std::vector<std::string> copy, save = input; 
+
+    //part1
+    while (input != copy){
+        
+        copy = input;
+        
         // now that we have border, can treat all relevant cells as the centre
         // loop through whole system except border
-        for (unsigned int y = 1; y<input.size()-1; y++){
-            for (unsigned int x = 1; x<input[0].size()-1; x++){
+        for (size_t y = 1; y<height; y++){
+            for (size_t x = 1; x<width; x++){
 
                 // if empty and all adjacent empty -> become full
                 if ( (copy[y][x] == 'L') && (copy[y+1][x] != '#') && (copy[y-1][x] != '#') && (copy[y][x+1] != '#') && (copy[y][x-1] != '#') && (copy[y+1][x+1] != '#') && (copy[y+1][x-1] != '#') && (copy[y-1][x+1] != '#') && (copy[y-1][x-1] != '#') ){
@@ -42,47 +46,164 @@ int main(){
                 else if ( copy[y][x] == '#' ){
                     int full_count = 0;
 
-                    if ( copy[y+1][x]   == '#' ){ full_count++; }
-                    if ( copy[y-1][x]   == '#' ){ full_count++; }
-                    if ( copy[y][x-1]   == '#' ){ full_count++; }
-                    if ( copy[y][x+1]   == '#' ){ full_count++; }
+                    if ( copy[y+1][x  ] == '#' ){ full_count++; }
+                    if ( copy[y-1][x  ] == '#' ){ full_count++; }
+                    if ( copy[y  ][x-1] == '#' ){ full_count++; }
+                    if ( copy[y  ][x+1] == '#' ){ full_count++; }
                     if ( copy[y+1][x-1] == '#' ){ full_count++; }
                     if ( copy[y-1][x-1] == '#' ){ full_count++; }
                     if ( copy[y+1][x+1] == '#' ){ full_count++; }
                     if ( copy[y-1][x+1] == '#' ){ full_count++; }
 
-                    if ( full_count >= 4 ){
-                        input[y][x] = 'L';
+                    if ( full_count >= 4 ){ input[y][x] = 'L'; }
+                }
+            }
+        }
+
+        //iter++;   
+    }
+
+    std::cout << "Answer (part 1): " << grid_count(input,'#') << std::endl;
+     
+    // reset grid
+    input = save;
+
+    while (input != copy){
+
+        copy = input;
+
+        for (size_t y = 1; y<height; y++){
+            for (size_t x = 1; x<width; x++){
+
+                // if not seat, ignore position
+                if ( copy[y][x] == '.' ){ continue; }
+
+                std::vector<bool> occupied(8,false);
+                
+                // directions
+                unsigned int a,b;
+
+                // west
+                a = x+1;
+                while (a!=copy[0].size()-1){
+
+                    if ( copy[y][a] == '#' ){
+                        occupied[0] = true;
+                        break;
                     }
+                    else if (copy[y][a] == 'L'){ break; }
+                    a++;
                 }
 
-            }
-        }
+                // east
+                a = x-1;
+                while (a!=0){
 
-        if ( input == copy ){
+                    if ( copy[y][a] == '#' ){
+                        occupied[1] = true;
+                        break;
+                    }
+                    else if (copy[y][a] == 'L'){ break; }
+                    a--;
+                }
 
-            int tally = 0;
+                // north
+                a = y-1;
+                while (a!=0){
 
-            // simulation has stabilised
-            for (unsigned int i=1; i<input.size()-1; i++){
-                for (unsigned int j=1; j<input[0].size()-1; j++){
+                    if ( copy[a][x] == '#' ){
+                        occupied[2] = true;
+                        break;
+                    }
+                    else if (copy[a][x] == 'L'){ break; }
+                    a--;
+                }
 
-                    if (input[i][j] == '#'){
-                        tally++;
-                    } 
+                // south
+                a = y+1;
+                while (a!=copy.size()-1){
+
+                    if ( copy[a][x] == '#' ){
+                        occupied[3] = true;
+                        break;
+                    }
+                    else if (copy[a][x] == 'L'){ break; }
+                    a++;
+                }
+
+                // north east
+                a = y-1;
+                b = x+1;
+                while (a!=0 && b!=copy[0].size()-1){
+
+                    if (copy[a][b] == '#' ){
+                        occupied[4] = true;
+                        break;
+                    }
+                    else if (copy[a][b] == 'L'){ break; }
+                    a--;
+                    b++;
+                }
+
+                // north west
+                a = y-1;
+                b = x-1;
+                while (a!=0 && b!=0){
+
+                    if (copy[a][b] == '#' ){
+                        occupied[5] = true;
+                        break;
+                    }
+                    else if (copy[a][b] == 'L'){ break; }
+                    a--;
+                    b--;
+                }
+
+                // south east
+                a = y+1;
+                b = x+1;
+                while (a!=copy.size()-1 && b!=copy[0].size()-1){
+
+                    if (copy[a][b] == '#' ){
+                        occupied[6] = true;
+                        break;
+                    }
+                    else if (copy[a][b] == 'L'){ break; }
+                    a++;
+                    b++;
+                }
+
+                // south west
+                a = y+1;
+                b = x-1;
+                while (a!=copy.size()-1 && b!=0){
+                    
+                    if (copy[a][b] == '#' ){
+                        occupied[7] = true;
+                        break;
+                    }
+                    else if (copy[a][b] == 'L'){ break; }
+                    a++;
+                    b--;
+                }
+
+                // tally full seats
+                int tally = 0;
+                for (bool state : occupied){
+                    if (state){ tally++; }
+                }
+
+                if ( (copy[y][x] == '#') && (tally >= 5) ){
+                    input[y][x] = 'L';
+                }
+                else if ( (copy[y][x] == 'L') && (tally == 0) ){
+                    input[y][x] = '#';
                 }
             }
-            
-            std::cout << "After " << iter << " iterations." << std::endl;
-            std::cout << "Number of occupied seats: " << tally << std::endl;
-            running = false;
-        }
-        else {
-            iter++;
-            copy = input;
         }
     }
 
+    std::cout << "Answer (part 2): " << grid_count(input,'#') << std::endl;
 
     return 0;
 }
--- week_2/day_13/day_13.cpp
+++ week_2/day_13/day_13.cpp
@@ -11,11 +11,12 @@ int main(){
 
     // read input into vector of strings.
     std::vector<std::string> input = read_input("input", ",");
+    size_t size = input.size();
 
     // vectors to store bus line and index
-    std::vector<long long int> bus_line, index; 
+    std::vector<long long> bus_line, index, time_diff; 
 
-    for (unsigned int i=1; i<input.size(); i++){
+    for (size_t i=1; i<size; i++){
         
         // ignore "x"
         if ( input[i] == "x" ){ continue; }
@@ -27,8 +28,22 @@ int main(){
         index.push_back( line- ((i-1) % line) );
     }
 
+    // resize vector to store values
+    time_diff.resize(bus_line.size());
+    int target = std::stoi(input[0]);
+
+    // calculate the number of minutes till bus leaves from target
+    for (size_t i=0; i<bus_line.size(); i++){
+        time_diff[i] = bus_line[i] - (target % bus_line[i]);
+    }
+
+    // find the minimum time difference ('it' is an iterator to the position of the min)
+    auto it = std::min_element(time_diff.begin(), time_diff.end());
+
+    std::cout << "Answer (part 1): " << bus_line[it - time_diff.begin()]*(*it)   << std::endl;
+
     // find the answer using chinese remainder theorem
-    long long int answer = chinese_remainder(bus_line, index);
+    long long answer = chinese_remainder(bus_line, index);
     
     std::cout << "Answer (part 2): " << answer << std::endl;
 
