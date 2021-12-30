d007cd39f7f7a303effb7b42bc9ef520a38f0d86
Daniel Meilak <daniel.mlak@gmail.com>
2021-12-30 23:00:45 +0100

    updated new_day.sh

--- new_day.sh
+++ new_day.sh
@@ -1,9 +1,31 @@
 #!/bin/bash
 
+#-------------------------------------------------------
 # script for making new day folder and including files
+#-------------------------------------------------------
 
-mkdir day_${1}
+# argument must be passed to script
+if [[ -z ${1+x} ]]; then
+    echo "A day number is required"
+    exit 1
+fi
 
+
+# determine week
+if   (( ${1} >= 1  && ${1} <= 7  )); then
+    week="week_1"
+elif (( ${1} >= 8  && ${1} <= 14 )); then
+    week="week_2"
+elif (( ${1} >= 15 && ${1} <= 21 )); then
+    week="week_3"
+else 
+    week="week_4"
+fi
+
+# create day folder (and week if necessary)
+mkdir -p ${week}/day_${1}
+
+# create template c++ file
 echo "#include<iostream>
 #include<vector>
 #include<string>
@@ -24,8 +46,9 @@ int main(){
     std::cout << \"Answer (part 2): \" << std::endl;
 
     return 0;
-}" >> day_${1}/day_${1}.cpp
+}" >> ${week}/day_${1}/day_${1}.cpp
 
+# create template c++ test file 
 echo "#include<iostream>
 #include<vector>
 #include<string>
@@ -36,7 +59,8 @@ echo "#include<iostream>
 int main(){
 
     return 0;
-}" >> day_${1}/test.cpp
+}" >> ${week}/day_${1}/test.cpp
 
-echo "## Day ${1}" >> day_${1}/README.md
+# create README.md for each day
+echo "## Day ${1}" >> ${week}/day_${1}/README.md
 
